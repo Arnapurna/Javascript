62f9bbe5ebbcd773c7698fdf36a00d671acb73e7
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-10-03 00:10:05 +0530

    Function in JS

---
+++ BASIC/05_function.js
@@ -0,0 +1,14 @@
+// Function is a set of statements , which is used to perform some task
+// To execute the function statement we have to call the function
+
+// Rules
+//  1) Function name should start with lowercase letter
+//      greet()
+//  2) Function name should not start with number
+//  3) If we are having multiple words for Function name, then every 2nd word 1st character should be uppercase\
+//      greetMsg()
+//      wishMe()
+function greet() {
+    console.log("Happy birthday");
+}
+greet();
