1c211ff109d83bc71583b533f517e8192034f9db
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-10-03 00:10:45 +0530

    Comment in JS

---
+++ BASIC/06_comment.js
@@ -0,0 +1,12 @@
+//  Comments are used to describe about the statement.
+//  There are 2 types of comments :-
+//      single line comment
+//      multi line comment
+
+
+// let a= 10;           // This is single line comment
+
+/*  
+    let a= 10;          // THis is multi line comment
+    console.log(a);
+*/ 
