f6b2e66296cd737dbe32e9a331d7392417d64cd8
Arnapurna Bedi <bediarnapurna20@gmail.com>
2022-10-25 19:49:33 +0530

    Operator in Js

---
+++ BASIC/18_Relational_operator.js
@@ -0,0 +1,15 @@
+// Relational operators
+// A relational operator compares its operands and returns a Boolean value based on whether the comparison is true.
+
+//  > (greater than)
+//  < (less than)
+//  >= (greater than or equal to)
+//  <== (less than or equal to)
+
+
+let a = 10;
+let b = 20;
+console.log(a > b);           //false
+console.log(a >= b);          //false
+console.log(a < b);           //true
+console.log(a <= b);          //true
